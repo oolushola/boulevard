1303b530e4c4ad424c0c1fa0615f4ba3ac8f0739
oolushola <odejobiolushola@gmail.com>
2021-04-06 10:04:44 +0100

    feature(login):authentication

---
+++ routes/auth/auth.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const { body } = require('express-validator')
+const multer = require('multer')
+const { uploadPhoto, User } = require('../../controllers/user')
+const UserModel = require('../../models/user')
+const router = express.Router()
+
+router.post(
+    '/signup',
+    [
+        body('firstName', 'first name is required').trim().isString().isLength({ min: 2, max: 40 }),
+        body('lastName', 'last name is required').trim().isString().isLength({ min: 2, max: 40}),
+        body('email', 'email is required and must be valid').isEmail().trim()
+            .custom((value, { req }) => {
+                return UserModel.findOne({ email: req.body.email })
+                    .then(user => {
+                        if(user) {
+                            return Promise.reject('user already exists')
+                        }
+                    })
+            })
+            .normalizeEmail(),
+        body('password', 'Password is required').isLength({ min: 8 }).trim(),
+        body('confirmPassword', 'Confirm password is required')
+            .custom((value, {req}) => {
+                if(value !== req.body.password) {
+                    throw new Error('Password does not match')
+                }
+            }),
+        body('phoneNo', 'Phone number is required').isMobilePhone().trim()
+    ],
+    uploadPhoto,
+    User.signUp
+)
+router.post(
+    '/login',
+    [
+        body('email')
+            .trim()
+            .isEmail()
+            .normalizeEmail(),
+        body('password', 'Password is required').isLength({ min: 8 }).trim(),
+    ],
+    User.login
+)
+
+module.exports = router
