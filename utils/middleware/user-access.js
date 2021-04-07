25270cf2afa17123c44669afe50a9b8d19b9e070
oolushola <odejobiolushola@gmail.com>
2021-04-07 13:22:34 +0100

    feature(user-status):update user status

--- utils/middleware/user-access.js
+++ utils/middleware/user-access.js
@@ -2,7 +2,23 @@ const userModel = require('../../models/user')
 
 class Priviledges { 
     static superAdminAccess(req, res, next) {
-        console.log(req.userType)
+        const userId = req.userId
+        userModel.findById(userId)
+            .then(user => {
+                if(user.userRole !== 'Super Admin') {
+                    const error = new Error('Access denied. You do not have permission to change user role')
+                    error.statusCode = 403
+                    next(error)
+                }
+                next()
+            })
+            .catch(err => {
+                if(!err.statusCode) {
+                    err.statusCode = 500
+                    next(err)
+                }
+            })
+        
     }
 }
 
