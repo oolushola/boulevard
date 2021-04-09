60a5a79d74a113de5f59c477b22d1290f3baf9ea
oolushola <odejobiolushola@gmail.com>
2021-04-09 12:48:51 +0100

    feature(truck-type):update a truck type

--- controllers/trucktype.js
+++ controllers/trucktype.js
@@ -1,9 +1,10 @@
 const TruckTypeModel = require('../models/preferences/trucktype')
 const responses = require('../utils/response')
 const { validationResult } = require('express-validator')
+const { response } = require('express')
 
 class TruckType {
-  static getLoadingSites(req, res, next) {
+  static getTruckTypes(req, res, next) {
     TruckTypeModel
       .find()
       .then(truckTypes => {
@@ -35,6 +36,46 @@ class TruckType {
       .catch(err => {
         responses.serverErrorResponse(err, 500, next)
       })
+  }
+
+  static getTruckType(req, res, next) {
+    const truckTypeId = req.params.truckTypeId
+    TruckTypeModel.findById(truckTypeId)
+      .then(truckType => {
+        if(!truckType) {
+          return responses.errorResponse(res, 404, 'resource not found')
+        }
+        return responses.successResponse(res, 200, 'truck type details', truckType)
+      })
+      .catch(err => {
+        responses.serverErrorResponse(err, 500, next)
+      })
+  }
+
+  static updateTruckType(req, res, next) {
+    const truckTypeId = req.params.truckTypeId
+    const errors = validationResult(req)
+    if(!errors.isEmpty()) {
+      return responses.errorResponse(res, 422, 'validation failed', errors.array())
+    }
+    TruckTypeModel.findById(truckTypeId)
+    .select('-__v -updatedAt -createdAt')
+    .then(matchedTruckType => {
+      if(!matchedTruckType) {
+        return responses.errorResponse(res, 404, 'resource not found')
+      }
+      const truckTypeCode = req.body.truckTypeCode
+      const truckType = req.body.truckType
+      matchedTruckType.truckTypeCode = truckTypeCode
+      matchedTruckType.truckType = truckType
+      return matchedTruckType.save()
+    })
+    .then(result => {
+      responses.successResponse(res, 200, 'truck type updated', result)
+    })
+    .catch(err => {
+      responses.serverErrorResponse(err, 500, next)
+    })
   }
 }
 
--- routes/preferences/trucktype.js
+++ routes/preferences/trucktype.js
@@ -8,7 +8,7 @@ const TruckTypeModel = require('../../models/preferences/trucktype')
 
 router.get(
   '/truck-types', 
-  truckTypeController.getLoadingSites
+  truckTypeController.getTruckTypes
 )
 
 router.post(
@@ -34,6 +34,41 @@ router.post(
         })
   ],
   truckTypeController.addTruckType
+)
+
+router.get(
+  '/truck-type/:truckTypeId',
+  truckTypeController.getTruckType
+)
+
+router.put(
+  '/truck-type/:truckTypeId',
+  verifyToken,
+  userAccess.admin,
+  [
+    body('truckTypeCode').trim().notEmpty().isLength({ min: 2, max: 4 }),
+    body('truckType')
+      .trim()
+      .notEmpty()
+      .custom((value, { req }) => {
+        return TruckTypeModel
+          .find({ _id: { $ne: req.params.truckTypeId} })
+          .or([
+            {
+              truckType: req.body.truckType,
+            },
+            {
+              truckTypeCode: req.body.truckTypeCode
+            }
+        ])
+          .then((truckType) => {
+            if(truckType.length > 0) {
+              return Promise.reject(`${value} or ${req.body.truckTypeCode} already exists`)
+            }
+          })
+        })
+  ],
+  truckTypeController.updateTruckType
 )
 
 module.exports = router
