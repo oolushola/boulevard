7692474feca5f1669f4aa91290a76aea12ba27fa
oolushola <odejobiolushola@gmail.com>
2021-04-09 11:49:39 +0100

    feature(product):create truck type

--- controllers/trucktype.js
+++ controllers/trucktype.js
@@ -1,5 +1,6 @@
 const TruckTypeModel = require('../models/preferences/trucktype')
 const responses = require('../utils/response')
+const { validationResult } = require('express-validator')
 
 class TruckType {
   static getLoadingSites(req, res, next) {
@@ -10,6 +11,26 @@ class TruckType {
           return responses.successResponse(res, 200, 'no resource found', truckTypes)
         }
         responses.successResponse(res, 200, 'truck type resource lists', truckTypes)
+      })
+      .catch(err => {
+        responses.serverErrorResponse(err, 500, next)
+      })
+  }
+
+  static addTruckType(req, res, next) {
+    const { truckTypeCode, truckType } = req.body
+    const errors = validationResult(req)
+    if(!errors.isEmpty()) {
+      return responses.errorResponse(res, 422, 'validation failed', errors.array())
+    }
+
+    const newTruckType = new TruckTypeModel({ 
+      truckTypeCode: truckTypeCode,
+      truckType: truckType
+    })
+    return newTruckType.save()
+      .then(result => {
+        responses.successResponse(res, 201, 'resource created successfully', result)
       })
       .catch(err => {
         responses.serverErrorResponse(err, 500, next)
--- routes/preferences/trucktype.js
+++ routes/preferences/trucktype.js
@@ -4,9 +4,36 @@ const { body } = require('express-validator')
 const truckTypeController = require('../../controllers/trucktype')
 const verifyToken = require('../../utils/middleware/verifyToken')
 const userAccess = require('../../utils/middleware/user-access')
+const TruckTypeModel = require('../../models/preferences/trucktype')
 
 router.get(
   '/truck-types', 
-  truckTypeController.getLoadingSites)
+  truckTypeController.getLoadingSites
+)
+
+router.post(
+  '/truck-type',
+  verifyToken,
+  userAccess.admin,
+  [
+    body('truckTypeCode').trim().notEmpty().isLength({ min: 2, max: 4 }),
+    body('truckType')
+      .trim()
+      .notEmpty()
+      .custom((value, { req }) => {
+        return TruckTypeModel
+          .find({ truckTypeCode: req.body.truckTypeCode })
+          .or([{
+            truckType: req.body.truckType
+          }])
+          .then((truckType) => {
+            if(truckType.length > 0) {
+              return Promise.reject(`${value} or ${req.body.truckTypeCode} already exists`)
+            }
+          })
+        })
+  ],
+  truckTypeController.addTruckType
+)
 
 module.exports = router
